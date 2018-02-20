8e0182a84cd1a14003366ccbb45300fc69dbf22a
scherng <sandy.cherng@gmail.com>
2018-02-20 12:06:39 -0800

    + show() have proper indents
    Details:
    + fixed & working: show() function indentation

--- src/main/java/lifelog/Main.java
+++ src/main/java/lifelog/Main.java
@@ -22,7 +22,7 @@ public class Main {
 	
 	public static void main (String[] args) throws IOException, ParseException, BadStringOperationException {
 		loadState();
-		showQuestions();
+		showAll();
 		//Tools.printTypeAndContent(category_hierarchy, "cat-h");
 		//Tools.printTypeAndContent(topic_hierarchy, "top-h");
 		//Tools.printTypeAndContjent(question_hierarchy, "que-h");
@@ -203,58 +203,20 @@ public class Main {
 
 	private static void showAll() {
 		StringBuilder sb = new StringBuilder();
-		appendCategories(sb, true, true);
+		appendCategories(sb, 2);
 		System.out.println(sb.toString());
-	}
-	
-	private static void appendCategories(StringBuilder sb, Boolean append_topics, Boolean append_questions) {
-		for (String category_id: category_hierarchy) {
-			sb.append(String.format("%1$s | %2$s\n", 
-					category_id, 
-					categories.get(category_id).prompt
-					));
-			if (append_topics) {
-				appendTopics(category_id, sb, append_questions);
-
-			}
-		}
-	}
-	
-	private static void appendTopics(String category_id, StringBuilder sb, Boolean append_questions) {
-		if (topic_hierarchy.containsKey(category_id)) {
-			for (String topic_id: topic_hierarchy.get(category_id)) {
-				sb.append((String.format("\t> %1$s (%2$s)\n",
-						topics.get(topic_id).prompt,
-						topic_id
-						)));
-				if (append_questions) {
-					appendQuestions(topic_id, sb);
-				}
-			}
-		}
-	}
-	
-	private static void appendQuestions(String topic_id, StringBuilder sb) {
-		if (question_hierarchy.containsKey(topic_id)) {
-			for (String question_id: question_hierarchy.get(topic_id)) {
-				sb.append((String.format("\t\tL %1$s (%2$s)\n",
-						questions.get(question_id).prompt,
-						question_id
-						)));
-			}
-		}
 	}
 	
 	private static void showCategories() {
 		StringBuilder sb = new StringBuilder();
-		appendCategories(sb, true, false);
+		appendCategories(sb, 1);
 		System.out.println(sb.toString());
 	}
 	
 	private static void showTopics() {
 		StringBuilder sb = new StringBuilder();
 		for (String category_id: category_hierarchy) {
-			appendTopics(category_id, sb, true);
+			appendTopics(sb, category_id, 1, 0);
 		}
 		System.out.println(sb.toString());		
 	}
@@ -263,12 +225,52 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for (LinkedList<String> topic_ll: topic_hierarchy.values()) {
 			for (String topic_id: topic_ll) {
-				appendQuestions(topic_id, sb);
+				appendQuestions(sb, topic_id, 0);
 			}
 		}
 		System.out.println(sb.toString());
 	}
 
+	private static void appendCategories(StringBuilder sb, int layers_to_show) { // layers = 0 means show only this layer
+		for (String category_id: category_hierarchy) {
+			sb.append(String.format("%1$s | %2$s\n", 
+					category_id, 
+					categories.get(category_id).prompt
+					));
+			if (layers_to_show > 0) {
+				appendTopics(sb, category_id, (layers_to_show - 1), 1);
+
+			}
+		}
+	}
+	
+	private static void appendTopics(StringBuilder sb, String category_id, int layers_to_show, int tabs) {
+		if (topic_hierarchy.containsKey(category_id)) {
+			for (String topic_id: topic_hierarchy.get(category_id)) {
+				sb.append((String.format("%1$s> %2$s (%3$s)\n",
+						String.join("", Collections.nCopies(tabs, "\t")), //repeat \t tabs times
+						topics.get(topic_id).prompt,
+						topic_id
+						)));
+				if (layers_to_show > 0) {
+					appendQuestions(sb, topic_id, (tabs + 1));
+				}
+			}
+		}
+	}
+	
+	private static void appendQuestions(StringBuilder sb, String topic_id, int tabs) {
+		if (question_hierarchy.containsKey(topic_id)) {
+			for (String question_id: question_hierarchy.get(topic_id)) {
+				sb.append((String.format("%1$sL %2$s (%3$s)\n",
+						String.join("", Collections.nCopies(tabs, "\t")), //repeat \t tabs times
+						questions.get(question_id).prompt,
+						question_id
+						)));
+			}
+		}
+	}
+	
 	private static void helpText(Console c) {
 		// TODO Auto-generated method stub
 		
